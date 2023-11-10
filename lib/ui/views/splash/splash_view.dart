942dcaf4b461fc6115633459ab98d8e296eb701a
Rushikesh <somvanshihrishikesh7@gmail.com>
2023-11-10 18:57:14 +0530

    Splash Screen

--- lib/ui/views/splash/splash_view.dart
+++ lib/ui/views/splash/splash_view.dart
@@ -12,7 +12,34 @@ class SplashView extends StatelessWidget {
       builder: (context, model, child) {
         return const Scaffold(
             body: Center(
-          child: Text("On Splash Screen"),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              // Add your photo here
+              Image(
+                image: AssetImage('assets/darpan_photo.png'),
+              ),
+
+              SizedBox(
+                  height:
+                      16.0), // Add some space between the photo and the text
+              Text(
+                'Darpan', // Your App name
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24.0,
+                ),
+              ),
+              SizedBox(height: 8.0),
+              Text(
+                'BVCOENM',
+                style: TextStyle(
+                  fontStyle: FontStyle.italic,
+                  fontSize: 18.0,
+                ),
+              ),
+            ],
+          ),
         ));
       },
     );
