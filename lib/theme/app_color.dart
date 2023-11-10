dccdbe147c20f0ae4329f2ace6610b0570f32250
Santosh Phadtare <santoshphadtare1610@gmail.com>
2023-11-10 14:28:21 +0530

    Fix: Changes done

---
+++ lib/theme/app_color.dart
@@ -0,0 +1,10 @@
+import 'package:darpan/file_exporter.dart';
+
+class ColorThemeClass {
+  static const primaryColor = Color(0xFF4E4BD7);
+  static const backgroundColor = Color(0xFFECECFF);
+  static const secondaryLPurpleColor = Color(0xFFD6D5FA);
+  static const secondaryWhiteColor = Color(0xFFF9F9F9);
+  static const secondaryBlackColor = Color(0xFF252525);
+  static const secondarySectionColor = Color(0xFF8F8FB0);
+}
