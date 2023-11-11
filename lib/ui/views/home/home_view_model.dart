cf84b0c4916e8faf6889eb160e964bdc35df16f1
Santosh Phadtare <santoshphadtare1610@gmail.com>
2023-11-11 11:33:51 +0530

    Fix: Font for department updates

--- lib/ui/views/home/home_view_model.dart
+++ lib/ui/views/home/home_view_model.dart
@@ -3,30 +3,46 @@ part of 'home_view.dart';
 class HomeViewModel extends BaseViewModel {
   final log = getLogger('HomeViewModel');
   final fontTheme = FontThemeClass();
-
+  final currentIndex =3;
   String user = "Sachin";
-  int currentIndex = 3;
 
   final PageController pageController = PageController();
   List<DepartmentUpdates> departmentUpdatesList = [
     DepartmentUpdates(
         name: "Placement",
         description:
-            "A TCS company will be arrived on 17 of December 2023.  This company require Software Develop",
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
         date: "Today"),
     DepartmentUpdates(
         name: "Placement",
         description:
-            "A TCS company will be arrived on 17 of December 2023.  This company require Software Develop",
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
         date: "20-08-23"),
     DepartmentUpdates(
         name: "Placement",
         description:
-            "A TCS company will be arrived on 17 of December 2023.  This company require Software Develop",
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
         date: "18-09-23"),
   ];
+
+  // Container expand controller
+  bool isExpanded = false;
+  var expandedHeight = 90.hWise;
+ var maxLines = 2;
+  void toggleExpand() {
+    isExpanded = !isExpanded;
+    if (isExpanded) {
+      expandedHeight = 200.hWise;
+      maxLines = 10;
+    } else {
+      expandedHeight = 90.hWise;
+      maxLines = 2;
+    }
+    notifyListeners();
+  }
 }
 
+// Deparament Updates Model
 class DepartmentUpdates {
   late String name;
   late String description;
