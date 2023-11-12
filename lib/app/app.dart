7f3b37e8ba78f7f6b62d432b1899f6917fe54001
Santosh Phadtare <santoshphadtare1610@gmail.com>
2023-11-12 12:13:10 +0530

    Feature: Added Event folder with files in project (Basic Layout)

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -3,6 +3,7 @@ import 'package:darpan/services/local_storage_service.dart';
 import 'package:darpan/theme/theme_service.dart';
 import 'package:darpan/ui/views/auth/auth_view.dart';
 import 'package:darpan/ui/views/bottom_nav/bottom_nav_view.dart';
+import 'package:darpan/ui/views/event/event_view.dart';
 import 'package:darpan/ui/views/profile/profile_view.dart';
 import 'package:stacked/stacked_annotations.dart';
 import 'package:stacked_services/stacked_services.dart';
@@ -15,7 +16,8 @@ import 'package:darpan/ui/views/splash/splash_view.dart';
     MaterialRoute(page: HomeView),
     MaterialRoute(page: AuthView),
     MaterialRoute(page: ProfileView),
-    MaterialRoute(page: BottomNavView)
+    MaterialRoute(page: BottomNavView),
+    MaterialRoute(page: EventView),
   ],
   dependencies: [
     LazySingleton(classType: NavigationService),
