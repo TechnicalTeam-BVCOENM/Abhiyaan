652815733cfaeebac969539bedf823a864445b19
Niranjan Dorage <niranjandorage@gmail.com>
2023-12-11 12:11:31 +0530

    url launcher for syllabus

--- lib/ui/sub_views/academics/subject_page/subject_page_view.dart
+++ lib/ui/sub_views/academics/subject_page/subject_page_view.dart
@@ -1,5 +1,4 @@
 import 'package:darpan/file_exporter.dart';
-import 'package:darpan/ui/common/common_component_model.dart';
 part 'subject_page_view_model.dart';
 
 class SubjectView extends StatelessWidget {
@@ -25,19 +24,10 @@ class SubjectView extends StatelessWidget {
                 ),
                 backgroundColor: context.colorScheme.backgroundColor,
               ),
-              body: Padding(
+              body: const Padding(
                 padding:
-                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                child: ElevatedButton(
-                    onPressed: () {
-                      UrlLauncher externalUrlHandler = UrlLauncher();
-                      externalUrlHandler.launchURL(
-                          "https://www.vidyalankar.org/engineering/assets/docs/be/computer-engineering-syllabus-sem-vii-mumbai-university.pdf");
-                    },
-                    child: const SizedBox(
-                        width: double.infinity - 100,
-                        height: 40,
-                        child: Center(child: Text("click here for Syllabus")))),
+                    EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: Text(""),
               ));
         });
   }
