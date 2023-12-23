c34bddf927de7f11e22be96ce865c95cbebead65
Santosh Phadtare <santoshphadtare1610@gmail.com>
2023-12-23 21:25:14 +0530

    feat: Community sections added

--- lib/ui/views/community/community_view.dart
+++ lib/ui/views/community/community_view.dart
@@ -12,7 +12,39 @@ class CommunityView extends StatelessWidget {
     return ViewModelBuilder<CommunityViewModel>.reactive(
         viewModelBuilder: () => CommunityViewModel(),
         builder: (context, model, child) {
-          return const Scaffold();
+          FontThemeClass fontThemeClass = FontThemeClass();
+          return Scaffold(
+            appBar: AppBar(
+              backgroundColor: context.colorScheme.backgroundColor,
+              leading: IconButton(
+                onPressed: () {
+                  Navigator.pop(context); // pop to home screen
+                },
+                icon: Icon(
+                  Icons.arrow_back_rounded,
+                  size: 30.sp,
+                  color: context.colorScheme.secondaryBlackColor,
+                ),
+              ),
+              title: Text(
+                "Community",
+                style: fontThemeClass.title(context,color: context.colorScheme.secondaryBlackColor),
+              ),
+              centerTitle: true,
+            ),
+            backgroundColor: context.colorScheme.backgroundColor,
+            body:  SafeArea(
+              minimum: const EdgeInsets.symmetric(horizontal: 18).r,
+              child: const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SectionText(title: "Blogs"),
+                  SectionText(title: "Departmental Clubs"),
+                  SectionText(title: "Qoute of the day"),
+                ],
+              ),
+            ),
+          );
         });
   }
 }
