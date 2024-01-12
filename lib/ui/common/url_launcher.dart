754be24613311aa139ad5d7fd1dfb71b4bf5cea6
psantosh16 <santoshphadtare1610@gmail.com>
2024-01-12 12:38:06 +0530

    fix: Email manifest modified

--- lib/ui/common/url_launcher.dart
+++ lib/ui/common/url_launcher.dart
@@ -1,4 +1,6 @@
 // For launching external urls
+import 'dart:math';
+
 import 'package:abhiyaan/file_exporter.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -13,10 +15,19 @@ class UrlLauncher {
   }
 
   launchEmail(String email) async {
+    String? encodeQueryParameters(Map<String, String> params) {
+  return params.entries
+      .map((MapEntry<String, String> e) =>
+          '${Uri.encodeComponent(e.key)}=${Uri.encodeComponent(e.value)}')
+      .join('&');
+}
     try {
       Uri sendMail = Uri(
         scheme: 'mailto',
         path: email,
+        query: encodeQueryParameters(<String, String>{
+          'subject': 'Regarding help in Abhiyaan App',
+        }),
       );
       if (await canLaunchUrl(sendMail)) {
         await launchUrl(sendMail, mode: LaunchMode.externalApplication);
